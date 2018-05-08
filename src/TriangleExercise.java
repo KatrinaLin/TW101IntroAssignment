f09da7832035f725bcf5c4d121686748ad695c3f
Katrina <zhaoyanlin2016@gmail.com>
2018-05-08 17:36:26 +0800

    feat: print a vertical line of n asterisk

--- src/TriangleExercise.java
+++ src/TriangleExercise.java
@@ -3,16 +3,26 @@ public class TriangleExercise {
     public static void main(String args[]) {
         printOneAsterisk();
         System.out.println();
-        printNAsterisk(8);
+        printHorizontalLine(8);
+        System.out.println();
+        printVerticalLine(3);
     }
 
     public static void printOneAsterisk() {
         System.out.print("*");
     }
 
-    public static void printNAsterisk(int n) {
+    public static void printHorizontalLine(int n) {
         for (int i = 0; i < n; i++) {
             printOneAsterisk();
         }
     }
+
+    public static void printVerticalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            printOneAsterisk();
+            System.out.println();
+        }
+    }
+
 }
